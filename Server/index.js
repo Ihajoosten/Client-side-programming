68ae883f78fc2986565104538766ab8b6f46cb21
Luc Joosten <lha.joosten@student.avans.nl>
2019-11-03 14:52:13 +0100

    can create local server now

--- Server/index.js
+++ Server/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const logger = require('./Config/config').logger
+
 
 
 const app = express();
 const port = 1999;
 
 
-app.listen(port, () => logger.info(`skoolworkshops app listening on port ${port}!`))
+app.listen(port, () => logger.info(`Server listening on port ${port}!`))
 
 module.exports = app
 
