b9733aa4977d3bdc81984035d5b173d292bbb56b
Luc Joosten <lha.joosten@student.avans.nl>
2019-11-04 22:41:17 +0100

    added basic navbar item

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap'
+import './assets/css/style.css'
 
 Vue.config.productionTip = false
 
