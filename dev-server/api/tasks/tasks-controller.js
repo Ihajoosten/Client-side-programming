15737666363a096b47d733c9d5d461b8313c3b15
Luc Joosten <lha.joosten@student.avans.nl>
2019-11-05 15:14:04 +0100

    Create tasks-controller.js

---
+++ dev-server/api/tasks/tasks-controller.js
@@ -0,0 +1,29 @@
+export function index(req, res) {
+    // Get all tasks
+    
+    return res.json();
+}
+
+export function create(req, res) {
+    // Create task
+    
+    return res.json();
+}
+
+export function update(req, res) {
+    // Update task
+
+    return res.json();
+}
+
+export function show(req, res) {
+    // Get task by ID
+    
+    return res.json();
+}
+
+export function remove(req, res) {
+    // Delete task
+
+    return res.json();
+}
