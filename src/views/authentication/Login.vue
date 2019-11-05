174c1c5586aa0f5b37f192630c3646c13cf5c6c1
Luc Joosten <lha.joosten@student.avans.nl>
2019-11-05 19:52:23 +0100

    Updated Login view to actually login on web app

--- src/views/authentication/Login.vue
+++ src/views/authentication/Login.vue
@@ -1,15 +1,27 @@
 <template>
   <div>
     <h1 class="text-center">Login</h1>
-    <form class="custom-form" v-on:submit="onSubmit">
+    <form class="custom-form" v-on:submit.prevent="onSubmit">
       <div class="offset-md-1">
         <div class="form-group">
           <label for="username">Username</label>
-          <input type="text" class="form-control" id="username" placeholder="Username" />
+          <input
+            v-model="username"
+            type="text"
+            class="form-control"
+            id="username"
+            placeholder="Username"
+          />
         </div>
         <div class="form-group">
           <label for="password">Password</label>
-          <input type="password" class="form-control" id="password" placeholder="Password" />
+          <input
+            v-model="password"
+            type="password"
+            class="form-control"
+            id="password"
+            placeholder="Password"
+          />
         </div>
       </div>
       <div class="form-group text-center">
@@ -20,16 +32,25 @@
 </template>
 
 <script>
-import * as auth from '../../services/AuthService.js'
+import * as auth from "../../services/AuthService.js";
 
 export default {
-    name: 'login',
-    methods: {
-        onSubmit: function(event) {
-            event.preventDefault();
-            auth.login();
-            this.$router.push({ name: 'home' });
-        }
+  name: "login",
+  data: function() {
+    return {
+      username: "",
+      password: ""
+    };
+  },
+  methods: {
+    onSubmit: async function() {
+      const user = {
+        username: this.username,
+        password: this.password
+      };
+      await auth.login(user);
+      this.$router.push({ name: "home" });
     }
-}
+  }
+};
 </script>
