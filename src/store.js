d2817d247f85316c6d1bde27df2737a4358213bd
Luc Joosten <lha.joosten@student.avans.nl>
2019-11-05 01:26:19 +0100

    added state management and login design

--- src/store.js
+++ src/store.js
@@ -1,15 +1,32 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import * as auth from './services/AuthService.js'
+
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    isLoggedIn: false,
+    apiURL: 'http://localhost:3000',
+    username: null,
+    userId: null
   },
   mutations: {
+    authenticate(state) {
+      state.isLoggedIn = auth.isLoggedIn();
+      if(state.isLoggedIn) {
+        state.username = auth.getUserName();
+        state.userId = auth.getUserId();
+      } else {
+        state.username = null;
+        state.userId = null;
+      }
+    }
   },
   actions: {
-  },
-  modules: {
+    authenticate(context) {
+      context.commit('authenticate')
+    }
   }
 })
