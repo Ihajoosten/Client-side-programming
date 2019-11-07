58e770cef09882371f8e94401109f0ced5b9cf92
Luc Joosten <lha.joosten@student.avans.nl>
2019-11-07 19:10:31 +0100

    deleted files

--- src/App.vue
+++ src/App.vue
@@ -2,41 +2,26 @@
   <div id="app">
     <Navbar />
     <div id="app-container">
-      <AppHeader />
-      <transition name="page" mode="out-in" v-if="!isLoading">
       <router-view></router-view>
-    </transition>
     </div>
     <Footer />
-    <AppLoader />
+
   </div>
 </template>
 
 <script>
 import Navbar from "./components/Navbar.vue";
 import Footer from "./components/Footer.vue";
-import AppHeader from "./components/AppHeader.vue";
-import AppLoader from "./components/AppLoading.vue";
-import { mapGetters } from "vuex"
 
 
 export default {
   name: "app",
   components: {
     Navbar,
-    Footer,
-    AppHeader,
-    AppLoader
+    Footer
   },
   beforeCreate() {
     this.$store.dispatch("authenticate");
-    this.$store.dispatch("fetchData");
-  },
-    computed: {
-    ...mapGetters({
-      isLoading: "isLoading"
-    })
   }
-
 };
 </script>
